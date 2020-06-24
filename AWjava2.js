4c62d1deb3312e54fba347328421724549665138
DanialYousefi <66389034+DanialYousefi@users.noreply.github.com>
2020-06-24 15:53:43 -0700

    Add files via upload

---
+++ AWjava2.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "700px";
+  
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.body.style.backgroundColor = "white";
+}
